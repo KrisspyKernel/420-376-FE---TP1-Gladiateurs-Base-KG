e9e4b62a4e2438eeabf14b0b37b987612da1219a
KrisspyKernel <90928021+KrisspyKernel@users.noreply.github.com>
2021-09-29 21:27:10 -0400

    màj

--- JeuGladiateurs/src/combat/AffichageEcran.java
+++ JeuGladiateurs/src/combat/AffichageEcran.java
@@ -10,14 +10,23 @@ public class AffichageEcran {
     // <editor-fold defaultstate="collapsed" desc="Méthodes d'affichage">
     public void afficherDebutCombat() {
         // TODO : Afficher le message du début du combat, tel que montré dans l'énoncé
+        System.out.println("\n<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>");
+        System.out.println(">>>>> Que le combat commence ! <<<<<");
+        System.out.println("<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>\n");
     }
     
     public void afficherSeparateurDeTour() {
         // TODO : Afficher un retour à la ligne, suivi d'une ligne d'astérisques, tel que montré dans l'énoncé
+        System.out.println();
+        System.out.println("**************************************");
     }
 
     public void afficherSeparateurInfosPerso() {
         // TODO : Afficher un retour à la ligne, suivi du séparateur "infos personnages", tel que montré dans l'énoncé
+        System.out.println();
+        System.out.println("----------------");
+        System.out.println("INFOS PERSONNAGE");
+        System.out.println("----------------");
     }
 
     public void afficheVictoire(Personnage personnage1, Personnage personnage2) {
